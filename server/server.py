196f2f4e7bedb580ff1ebc372142a9d82900fcf4
SHY <ekdmbzzang@naver.com>
2022-11-04 22:46:12 +0900

    [CHORE] server settings

--- server/server.py
+++ server/server.py
@@ -1,11 +1,13 @@
 from flask import Flask
+import time
 
 app = Flask(__name__)
 
-@app.route('/')
-def users():
-    return {"members": [{ "id" : 1, "name" : "yerin" },
-            { "id" : 2, "name" : "dalkong" }]}
+@app.route('/api/v1/')
+def main():
+    return {
+        "time" : time.time(),
+    }
 
 if __name__ == "__main__":
     app.run(debug = True)
