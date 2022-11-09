1fbae3c68c66aadcab1c3aeeef529591688f729e
JooonJung <xodhkd102@naver.com>
2022-11-09 13:41:30 +0900

    [FEAT] Adding session time

--- server/config.py
+++ server/config.py
@@ -1,6 +1,8 @@
 import os
+from datetime import timedelta
 
 BASE_DIR = os.path.dirname(__file__)
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'JFP.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+PERMANENT_SESSION_LIFETIME = timedelta(days=7)
