6ae90f976305f57e2b506224eef380eade45b6eb
SHY <ekdmbzzang@naver.com>
2022-11-12 22:44:19 +0900

    [FEAT] Form - account form, change password form

--- server/Forms.py
+++ server/Forms.py
@@ -1,7 +1,8 @@
 from wtforms import StringField, PasswordField, EmailField, Form
-from wtforms.validators import DataRequired, Length, EqualTo, Email, StopValidation
+from wtforms.validators import DataRequired, Length, EqualTo, Email, StopValidation, ValidationError
 from models import User
 from werkzeug.security import check_password_hash
+from flask import session
 
 
 class RegisterForm(Form):
@@ -31,16 +32,60 @@ class LoginForm(Form):
 
 
 class AccountForm(Form):
+    def compareUsername(self, field):
+        user = User.query.filter_by(id=session['user_id']).first()
+        if user.username == field.data:
+            raise StopValidation(message='same username')
+
     username = StringField('username', validators=[
-        DataRequired(message="missing username"), 
+        DataRequired("missing username"),
         Length(min=2, max=25),
-        ])
+        compareUsername,
+        ]
+    )
+
+
+class PasswordChangeForm(Form):
+    class NotEqualTo(EqualTo):
+        def __init__(self, fieldname, message=None):
+            super().__init__(fieldname, message)
+
+        def __call__(self, form, field):
+            try:
+                other = form[self.fieldname]
+            except KeyError as exc:
+                raise ValidationError(
+                    field.gettext("Invalid field name '%s'.") % self.fieldname
+            ) from exc
+            if field.data != other.data:
+                return
+
+            d = {
+                "other_label": hasattr(other, "label")
+                and other.label.text
+                or self.fieldname,
+                "other_name": self.fieldname,
+            }
+            message = self.message
+            if message is None:
+                message = field.gettext("Field must not be equal to %(other_name)s.")
+
+            raise ValidationError(message % d)
+
+    oldPassword = PasswordField('oldPassword', validators=[
+            DataRequired("missing old password")
+        ]
+    )
+    newPassword = PasswordField('newPassword', validators=[
+            DataRequired("missing new password"),
+            NotEqualTo('oldPassword', 'password has not changed'),
+        ]
+    )
+
+
+class EmailForm(Form):
     email = EmailField('email', validators=[
         DataRequired(message="missing email"), 
         Email(),
-        ])
-    password = PasswordField('password', validators=[
-        DataRequired("missing password"), 
-        EqualTo('passwordConfirm', 'wrong password confirmation'),
-        ])
-    passwordConfirm = PasswordField('passwordConfirm', validators=[DataRequired("missing passwordConfirm")])
+        ]
+    )
