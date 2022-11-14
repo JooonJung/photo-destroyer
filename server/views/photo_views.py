74799fd3e50f937395a9fb2a80a8a3054aa65c12
SHY <ekdmbzzang@naver.com>
2022-11-14 11:36:14 +0900

    [FEAT] photo detail views : get, delete

--- server/views/photo_views.py
+++ server/views/photo_views.py
@@ -16,5 +16,38 @@ def photos():
     return make_response({"photos": [photo.serialize for photo in user.photo_user]}, 200)
 
 
+@bp.route('/<photo_id>', methods = ["GET", "DELETE"])
+def photosDetail(photo_id):
+  if request.method == "GET":
+    if 'user_id' not in session:
+      return make_response({"error": "no session"}, 401)
+    
+    photo = Photo.query.filter(Photo.id==photo_id).first()
+    if not photo:
+      return make_response({"error": "no matching photo"}, 401)
+
+    if photo.user_id != session['user_id']:
+      return make_response({"error": "not owner of photo"}, 401)
+
+    return make_response({"photo": photo.serialize}, 200)
+
+
+  if request.method == "DELETE":
+    if 'user_id' not in session:
+      return make_response({"error": "no session"}, 401)
+    
+    photo = Photo.query.filter(Photo.id==photo_id).first()
+    if not photo:
+      return make_response({"error": "no matching photo"}, 401)
+
+    if photo.user_id != session['user_id']:
+      return make_response({"error": "not owner of photo"}, 401)
+
+    db.session.delete(photo)
+    db.session.commit()
+
+    return make_response({"success": "photo delete success"}, 200)
+
+
 if __name__ == "__main__":
     bp.run(debug = True)
