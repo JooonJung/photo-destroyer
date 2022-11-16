d1eb1292f271cb4c6fd5850aefcfd1d31f8639e5
SHY <ekdmbzzang@naver.com>
2022-11-16 14:20:35 +0900

    [FEAT] photo create, tags update
    without form validate

--- server/views/photo_views.py
+++ server/views/photo_views.py
@@ -3,7 +3,7 @@ from db import db
 from models import User, Photo
 from flask import Blueprint, make_response, request, session
 from werkzeug.utils import secure_filename
-from utils import strTagToTagsList, saveImageAndReturnUrl
+from utils import strTagToTagsList, saveImageAndReturnUrl, tagsListToStrTag
 import time
 import mimetypes
 
@@ -31,7 +31,7 @@ def photos():
     brand = request.form["brand"]
     filename = f'{session["user_id"]}_{brand}_{"_".join(str(time.time()).split("."))}'
 
-    imgUrl = saveImageAndReturnUrl(QRcodeUrl=QRcodeUrl, brand=brand, filename=filename)    
+    imgUrl = saveImageAndReturnUrl(QRcodeUrl=QRcodeUrl, brand=brand, filename=filename)
     mimetype = mimetypes.guess_type(imgUrl)[0]
     tags = strTagToTagsList(request.form['tags'])
     owner_id = session['user_id']
@@ -59,6 +59,25 @@ def photosDetail(photo_id):
     return make_response({"photo": photo.serialize}, 200)
 
 
+  if request.method == "POST":
+    ''' Receive tags '''
+    ## TODO : form validation
+    if 'user_id' not in session:
+      return make_response({"error": "no session"}, 401)
+
+    photo = Photo.query.filter(Photo.id==photo_id).first()
+    if not photo:
+      return make_response({"error": "no matching photo"}, 401)
+
+    if photo.owner_id != session['user_id']:
+      return make_response({"error": "not owner of photo"}, 401)
+
+    tags = strTagToTagsList(request.form['tags'])
+    photo.tags = tagsListToStrTag(tags)
+    db.session.commit()
+    return make_response({"photo": photo.serialize}, 200)
+
+
   if request.method == "DELETE":
     if 'user_id' not in session:
       return make_response({"error": "no session"}, 401)
@@ -67,7 +86,7 @@ def photosDetail(photo_id):
     if not photo:
       return make_response({"error": "no matching photo"}, 401)
 
-    if photo.user_id != session['user_id']:
+    if photo.owner_id != session['user_id']:
       return make_response({"error": "not owner of photo"}, 401)
 
     db.session.delete(photo)
@@ -75,23 +94,6 @@ def photosDetail(photo_id):
 
     return make_response({"success": "photo delete success"}, 200)
 
-@bp.route('/upload', methods = ['POST'])
-def photoUpload():
-  if 'user_id' not in session:
-    return make_response({"error" : "no session"}, 401)
-  
-  file = request.files['file']
-  mimetype = file.mimetype
-  imgUrl = './static/upload/' + secure_filename(file.filename)
-  tags = list(request.form['tags'])
-  file.save(imgUrl)
-  owner_id = session['user_id']
-
-  photo = Photo(imgUrl=imgUrl, owner_id=owner_id, mimetype=mimetype, tags=tags)
-  db.session.add(photo)
-  db.session.commit()
-
-  return make_response({"success":"photo uploaded"}, 200)
 
 if __name__ == "__main__":
     bp.run(debug = True)
