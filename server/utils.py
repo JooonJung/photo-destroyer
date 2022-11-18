804d3fcf9512931341aa0a767eae059ad153cf68
SHY <ekdmbzzang@naver.com>
2022-11-19 00:17:26 +0900

    [FEAT] saveImageFromPhotoism function

--- server/utils.py
+++ server/utils.py
@@ -8,11 +8,14 @@ def formatTagsList(tagsList):
       tagsList[i] = "#" + tag
   return tagsList
 
+
 def strTagToTagsList(tags):
   return ast.literal_eval(tags)
 
+
 def tagsListToStrTag(tagsList):
   return f'{list(set(formatTagsList(tagsList)))}'
+
 
 def saveImageFromLifeFourCuts(url, filename):
   ''' 인생 네컷 링크 사진 저장'''
@@ -22,7 +25,24 @@ def saveImageFromLifeFourCuts(url, filename):
   req.urlretrieve(formattedUrl, BASE_DIR + f"/static/upload/{filename}.jpg")
 
 
+def saveImageFromPhotoism(url, filename):
+  ''' 포토이즘 링크 사진 저장 '''
+  ''' 5회까지만 URL 방문 가능 '''
+
+  splitUrl = url.split("/")
+  photoId = splitUrl[-1].split("=")[-1]
+  splitUrl.pop()
+  splitUrl.append("take")
+  splitUrl.append(photoId + ".jpg")
+  formattedUrl = "/".join(splitUrl)
+  req.urlretrieve(formattedUrl, BASE_DIR + f"/static/upload/{filename}.jpg")
+
+
 def saveImageAndReturnUrl(brand, QRcodeUrl, filename):
   if brand == "lifeFourCuts":
-      saveImageFromLifeFourCuts(QRcodeUrl, filename)
-      return f"./static/upload/{filename}.jpg"
+    saveImageFromLifeFourCuts(QRcodeUrl, filename)
+  elif brand == "photoism":
+    saveImageFromPhotoism(QRcodeUrl, filename)
+  
+  return f"./static/upload/{filename}.jpg"
+
